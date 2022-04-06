97d8a73c5a3caaa7c0b8e644284a4afebe2e9e0f
sayyad khadar basha <khadarsayyad786@gmail.com>
2022-04-06 09:33:10 +0530

    pug template engine

--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,19 @@ const express=require('express');
 const rootDir=require('../util/util');
 const router=express.Router();
 
+const products=[];
+
 router.get('/add-product',(req,res,next)=>{
   res.sendFile(path.join(rootDir,'views','add-product.html'));
 });
 
 router.post('/add-product',(req,res,next)=>{
-  console.log(JSON.parse(JSON.stringify(req.body)));
+  //console.log(JSON.parse(JSON.stringify(req.body)));
+  let formData=JSON.parse(JSON.stringify(req.body));
+  products.push({title:formData.title});
   res.redirect('/');
 });
 
-module.exports=router;
+//module.exports=router;
+exports.routes=router;
+exports.products=products;
