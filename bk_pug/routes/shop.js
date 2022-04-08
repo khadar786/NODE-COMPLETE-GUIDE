2b5b18099371080c899e4cd779b342edc1d0283d
sayyad khadar basha <khadarsayyad786@gmail.com>
2022-04-08 09:14:10 +0530

    Complete the Pug

--- bk_pug/routes/shop.js
+++ bk_pug/routes/shop.js
@@ -9,7 +9,7 @@ router.get('/',(req,res,next)=>{
   //console.log(adminData.products);
   //res.sendFile(path.join(rootDir,'views','shop.html'));
   const products=adminData.products;
-  res.render('shop',{prods:products,pageTitle:'Shop',path:'/'});
+  res.render('shop',{prods:products,docTitle:'Shop'});
 });
 
 module.exports=router;
